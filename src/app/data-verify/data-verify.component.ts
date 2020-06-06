f3f8713dc84d7ab5d605abd09ccc5cba08f0908c
Uppi28 <upendrabr.93@gmail.com>
2020-06-06 19:40:19 +0530

    data verify component WIP

---
+++ src/app/data-verify/data-verify.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-verify',
+  templateUrl: './data-verify.component.html',
+  styleUrls: ['./data-verify.component.scss']
+})
+export class DataVerifyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
