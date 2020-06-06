6dfbbff6d94c9bab8a8362c6dc926f97150fbc70
Uppi28 <upendrabr.93@gmail.com>
2020-06-06 17:39:29 +0530

    Header -WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
-  { path: 'header', component: HeaderComponent}
+  { path: 'header', component: HeaderComponent},
+  { path: '', component: MainComponent}
 ];
 
 @NgModule({
