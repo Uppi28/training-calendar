692de36771a8bc186fb1fec452708951da6c9831
Uppi28 <upendrabr.93@gmail.com>
2020-06-07 21:23:58 +0530

    questionnaire view

---
+++ src/assets/data/questions.js
@@ -0,0 +1,42 @@
+export const questions = [
+    {
+        "question": "Would you like to register for Course 1 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 2 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 3 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 4 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 5 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 6 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 7 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 8 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 9 ?",
+        "option": ["Yes", "No"]
+    },
+    {
+        "question": "Would you like to register for Course 10 ?",
+        "option": ["Yes", "No"]
+    }
+]
