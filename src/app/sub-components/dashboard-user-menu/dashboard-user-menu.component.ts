ab6646545d4a9e38a15915949fb25de51148beba
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-06-07 18:37:35 +0530

    dashboard - user menu - done

--- src/app/sub-components/dashboard-user-menu/dashboard-user-menu.component.ts
+++ src/app/sub-components/dashboard-user-menu/dashboard-user-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-user-menu',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardUserMenuComponent implements OnInit {
 
-  constructor() { }
+  public userMenu = [{
+    label: "My Home",
+    icon_name: "home",
+    isSelected: true,
+    isDisabled: false,
+    router: "home"
+  },
+  {
+    label: "Calendar",
+    icon_name: "calendar_today",
+    isSelected: false,
+    isDisabled: false,
+    router: "home"
+  },
+  {
+    label: "Analytics",
+    icon_name: "bar_chart",
+    isSelected: false,
+    isDisabled: false,
+    router: "home"
+  },
+  {
+    label: "Training",
+    icon_name: "list_alt",
+    isSelected: false,
+    isDisabled: false,
+    router: "home"
+  }]
+
+
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  menuSelected(selected) {
+    this.userMenu.forEach(menu => {
+      if (selected.label === menu.label)
+        menu.isSelected = true;
+      else
+        menu.isSelected = false;
+    })
+    this.router.navigate([selected.router])
   }
 
 }
